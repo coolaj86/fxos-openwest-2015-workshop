b7b416c428abde15f648466abb3505136894d168
AJ ONeal <awesome@coolaj86.com>
2015-05-09 15:29:16 -0600

    play with device motion

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ window.addEventListener("load", function() {
 
 // We name our App 'FxosDemo'
 angular.module('FxosDemo', [])
-  .controller('FxosController', [function () {
+  .controller('FxosController', ['$scope', function ($scope) {
       var FXC = this;
     
       FXC.sendSms = function (number, message) {
@@ -13,6 +13,24 @@ angular.module('FxosDemo', [])
         console.log('complete');
       }
       
+      FXC.motion = '{ "message": "Not Initialized" }';
+      window.addEventListener('devicemotion', function (event) {
+        window.motion = event;
+        
+        var rot = event.rotationRate;
+        //console.log('motion event', event);
+        
+        FXC.motion = JSON.stringify({
+          beta: rot.beta
+        , alpha: rot.alpha
+        , gamma: rot.gamma
+        , rot: rot
+        , grav: event.accelerationIncludingGravity
+        , acc: event.acceleration
+        });
+        
+        $scope.$apply();
+      });
       console.log("Hello Angular!");
   }])
   ;
