e254d08a07a56544bfcdd01b5e0f6d3fd5c460ac
AJ ONeal <awesome@coolaj86.com>
2015-05-09 14:35:29 -0600

    add angular and bootstrap

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 window.addEventListener("load", function() {
   console.log("Hello World!");
 });
+
+// We name our App 'FxosDemo'
+angular.module('FxosDemo', [])
+  .controller('FxosController', [function () {
+      console.log("Hello Angular!");
+  }])
+  ;
